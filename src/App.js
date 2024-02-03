98b8983c5f26fc2b4e0878ec4dcb7599c5ee233e
Gabriel Felippe <gabrielalexfelippe@gmail.com>
2024-02-02 21:17:10 -0300

    Added first links

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Inicio from "./paginas/Inicio";
 import SobreMim from "./paginas/SobreMim";
+import Menu from "./componentes/Menu";
 
-console.log(window.location)
-
-const pagina = window.location.pathname === '/' ? <Inicio /> : <SobreMim />
 
 function App() {
   return (
-    pagina
+    <BrowserRouter>
+
+      <Menu />
+
+      <Routes>
+        <Route path="/" element={<Inicio />} />
+        <Route path="/sobremim" element={<SobreMim />} />
+        <Route path="*" element={<div> Pagina não encontrada </div>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
